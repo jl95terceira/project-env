741ceeac411409bd49d980cd2c6c9823c8e75d3e
Silva Joao <jl95terceira@gmail.com>
2024-10-28 15:37:11 +0000

    (no message)

--- project/package/envlib/vars/builtin.py
+++ project/package/envlib/vars/builtin.py
@@ -23,33 +23,6 @@ EDITOR           = var(name       ='editor',
                        description='default text file editor',
                        type       =_editor,
                        default   =_editor('notepad'))
-GIT_HOME         = var(name       ='git.home', 
-                       description='the home of Git')
-GIT              = var(name       ='git', 
-                       description='the path to Git (\'git.exe\')',
-                       default    =os.path.join(GIT_HOME.get(), 'bin', 'git') if GIT_HOME.check() else 'git')
-GIT_REMOTE       = var(name       ='git.remote', 
-                       description='the alias of git remote that is commonly used (usually \'origin\')',
-                       default    ='origin')
-CURL             = var(name       ='curl', 
-                       description='the path / alias to Curl executable',
-                       default    ='curl')
-JDK_HOMES        = var(name       ='jdk.homes', 
-                       description='a map (dict) of Java homes by version (as a string)')
-MAVEN_HOME       = var(name       ='maven.home', 
-                       description='the home of Apache Maven')
-MAVEN            = var(name       ='maven', 
-                       description='the path / alias to Apache Maven executable',
-                       default    = os.path.join(MAVEN_HOME.get(), 'bin', 'mvn.exe') if MAVEN_HOME.check() else 'mvn')
-ANT_HOME         = var(name       ='ant.home', 
-                       description='the home of Apache Ant')
-ANT              = var(name       ='ant', 
-                       description='the path / alias to Apache Ant executable',
-                       default    = os.path.join(ANT_HOME.get(), 'bin', 'mvn.exe') if ANT_HOME.check() else 'ant')
-AZURE_DEVOPS_PAT = var(name       ='azure.devops.pat', 
-                       description='personal access token for Azure DevOps (artifact download permission etc)')
-OPENSSL_HOME     = var(name       ='openssl.home', 
-                       default    ='the home of OpenSSL')
 TEMP             = var(name       ='temp', 
                        description='a directory that may be used to hold temporary files',
                        default    =batt.os.TEMP_DIR)
